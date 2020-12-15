54634bc8b8f2c255fad838bd079c2e5d2fe44cfe
habibcse009 <habibcse009@gmail.com>
2020-12-16 04:22:34 +0600

    fast commit

---
+++ app/src/main/java/com/project/adunik_krisi/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.project.adunik_krisi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
